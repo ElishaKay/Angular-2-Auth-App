9c1da6d6009e07fc72f3cae78fd7960c071d0fac
Elisha Kramer <Kramer1346@gmail.com>
2017-02-27 21:15:12 +0200

    hey

---
+++ app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: '2F9L3F0Zt2Ctu10KrxZZjaUYDkUDFPj7',
+    domain: 'amp.eu.auth0.com'
+};
